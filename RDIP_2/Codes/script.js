590827f76a9bf527469d41feaaa149a5ea536705
Ayush Deep Nigam <aayushdeepnigam@gmail.com>
2020-07-22 02:18:27 +0530

    Goal 4: Display message and table

--- RDIP_2/Codes/script.js
+++ RDIP_2/Codes/script.js
@@ -16,16 +16,14 @@ var hinCorp = ["राम ने सीता के लिए फल तोड़�
 var corp = [];
 var senID = "";
 var lang = 0;
-var chosen = "";
+var chosenLine = "";
+var chosenArr = [];
 //--------------------------//
 
 
 function start_lang(){
   var langUsr = document.getElementById("lang").value;
-  document.getElementById("engSen").value = "null";
-  document.getElementById("hinSen").value = "null";
-  document.getElementById("engSen").style = "display: none";
-  document.getElementById("hinSen").style = "display: none";
+  reset();
   switch(langUsr){
     case "1":
       corp = engCorp;
@@ -48,8 +46,62 @@ function start_lang(){
   }
 }
 
+function reset(){  
+  document.getElementById("engSen").value = "null";
+  document.getElementById("hinSen").value = "null";
+  document.getElementById("engSen").style = "display: none";
+  document.getElementById("hinSen").style = "display: none";
+  document.getElementById("POSmsg").style = "display: none";
+  document.getElementById("tableDiv").style = "display: none";
+}
+
 function sentence(){
   var ind = document.getElementById(senID).value;
-  chosen = corp[ind];
-  alert(chosen);
+  chosenLine = corp[ind];
+  chosenLine = chosenLine.replace("।", "");
+  chosenLine = chosenLine.replace(".", "");
+  chosenArr = chosenLine.split(' ');
+  document.getElementById("POSmsg").style = "display: block";
+  document.getElementById("POSmsg").style = "color: blue";
+  document.getElementById("tableDiv").innerHTML = "";
+  createTable();
+}
+
+function createTable(){
+  document.getElementById("tableDiv").style = "display: block";
+  var tableDiv = document.getElementById("tableDiv");
+  var table = document.createElement("table");
+  var header = table.createTHead();
+  var row = header.insertRow(0);
+  var cell = row.insertCell(0);
+  cell.innerHTML = "LEXICON";
+  for (i=0; i<chosenArr.length;i++) {
+    var tr = document.createElement('tr');
+    var row1 = document.createElement('td');
+    var lexicon = document.createTextNode(chosenArr[i]);
+    row1.appendChild(lexicon);
+    tr.appendChild(row1);
+    table.appendChild(tr);
+
+    // var tr = document.createElement('tr');   
+
+    var td1 = document.createElement('td');
+    var td2 = document.createElement('td');
+    var td3 = document.createElement('td');
+
+    var text1 = document.createTextNode('');
+    var text2 = document.createTextNode('');
+    var text3 = document.createTextNode('');
+
+    td1.appendChild(text1);
+    td2.appendChild(text2);
+    td3.appendChild(text3);
+    tr.appendChild(td1);
+    tr.appendChild(td2);
+    tr.appendChild(td3);
+
+    table.appendChild(tr);
+
+   }
+  tableDiv.appendChild(table);
 }
